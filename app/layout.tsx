acc740d17cfa527384ed5d28a26100efced3f3c1
youssefw <ywtalaat@gmail.com>
2024-01-26 20:13:05 +0200

    tests

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import "@mantine/core/styles.css";
 import React from "react";
 import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import { theme } from "../theme";
+import "./global.css"
 
 export const metadata = {
   title: "Mantine Next.js template",
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,36 @@
+import { Button, TabsList, TabsPanel, TabsTab, Text } from '@mantine/core';
+import { Tabs, rem } from '@mantine/core';
 export default function HomePage() {
-  return <div>Home page</div>;
+
+  return <>
+    <p className='bg-purple-300'>Home page</p>
+    <Button className='bg-green-300 font-extrabold text-xl'>test</Button>
+    <Text className='bg-red-500'>ewfwefwefwe</Text>
+
+    <Tabs defaultValue="gallery">
+      <TabsList >
+        <TabsTab value="gallery" >
+          Gallery
+        </TabsTab>
+        <TabsTab value="messages">
+          Messages
+        </TabsTab>
+        <TabsTab value="settings" >
+          Settings
+        </TabsTab>
+      </TabsList>
+
+      <TabsPanel value="gallery">
+        Gallery tab content
+      </TabsPanel>
+
+      <TabsPanel value="messages">
+        Messages tab content
+      </TabsPanel>
+
+      <TabsPanel value="settings">
+        Settings tab content
+      </TabsPanel>
+    </Tabs>
+  </>;
 }
